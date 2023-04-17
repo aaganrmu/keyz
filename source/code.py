64151723d00274ceb2048e9a524b6e2d46047678
Elmar Wiedemeijer <elmar.wiedemeijer@gmail.com>
2023-04-17 22:58:12 +0200

    first try

--- source/code.py
+++ source/code.py
@@ -1,44 +1,34 @@
 import board
 import digitalio
+import keypad
 import time
 import usb_hid
-from keyz.key import Key
 
 led_onboard = digitalio.DigitalInOut(board.LED)
 led_onboard.direction = digitalio.Direction.OUTPUT
 
 pins = [
-        [board.GP6, 'shift'],
-        [board.GP7, 'a'],
-        [board.GP8, 'b'],
-        [board.GP9, 'c']
+        board.GP6,
+        board.GP7,
+        board.GP8,
+        board.GP9
        ]
-keys = []
 
-for pin in pins:
-    key = Key(pin[1], pin[0])
-    keys.append(key)
-
-keys[0].modifier = True
+keys = keypad.Keys(
+        pins=pins,
+        value_when_pressed=False,
+        pull=True,
+        max_events=10
+       )
 
 i = 0
-while i < 1000:
-    string = ""
-    for key in keys:
-        if key.poll():
-            if keys[0].state:
-                times = 2
-            else:
-                times = 1
-            for i in range(times):
-                led_onboard.value = 1
-                time.sleep(0.1)
-                led_onboard.value = 0
-                time.sleep(0.1)
-            
-    i += 1
-    time.sleep(0.01)
+while i<100:
 
+time.sleep(1)
+led_onboard.on()
+time.sleep(1)
+led_onboard.off()
+time.sleep(1)
 led_onboard.on()
 time.sleep(1)
 led_onboard.off()
