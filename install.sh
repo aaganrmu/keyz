a2efbfc10e7f75b43a4c2cf9a79dd3f4bdcf7e1c
Elmar Wiedemeijer <elmar.wiedemeijer@gmail.com>
2024-07-22 22:56:31 +0200

    chore: made script simpler

--- install.sh
+++ install.sh
@@ -2,28 +2,10 @@
 
 DEVICE="/media/elmarw/KEYZ/"
 
-echo "cleaning"
-remotefiles=$(ls $DEVICE)
-for file in $remotefiles; do
-    if [ "$file" = "keyz" ]; then
-        echo "found keyz install, removing"
-        $(rm -rf $DEVICE/keyz)
-    fi
-done
-
 echo "installing"
 cd source
-echo "transfering boot.py"
-$(cp boot.py $DEVICE)
-echo "transfering code.py"
-$(cp code.py $DEVICE)
-echo "transfering config"
-$(cp config $DEVICE)
-echo "creating folders"
-$(mkdir $DEVICE/keyz)
-cd keyz
 localfiles=$(ls)
 for file in $localfiles; do
-    echo "transfering keyz/$file"
-    $(cp $file $DEVICE/keyz)
+    echo "transfering $file"
+    $(cp -r $file $DEVICE/)
 done
